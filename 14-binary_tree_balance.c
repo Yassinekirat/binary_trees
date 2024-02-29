c722ccd197e528d0a9da38b6a9457e129d7a73dd
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-02-29 22:47:06 +0100

    task 14. Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,3 +19,32 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (balance);
 
 }
+
+#include "binary_trees.h"
+
+/**
+* binary_tree_height - Measures the height of a binary tree.
+*
+* @tree: Pointer to the root node of the tree to traverse.
+*
+* Return: Height.
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right, height;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree && !tree->left && !tree->right)
+	{
+		return (0);
+	}
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	height = MAX(left, right) + 1;
+
+	return (height);
+}
