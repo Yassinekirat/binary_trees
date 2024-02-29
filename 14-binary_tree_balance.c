6d48f287970994207b0cc30cf62260d5e9417132
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-02-29 12:16:25 +0100

    adding just the functions for now

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+* binary_tree_balance - measures the balance factor of a binary tree
+* @tree: Pointer to the root node of the tree to traverse.
+*
+* Return: If tree is NULL, return 0
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	return (0);
+}
---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_full - checks if a binary tree is full
+*
+* @tree: Pointer to the root node of the tree to travers
+*
+* Return: If tree is NULL, your function must return 0
+*/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	return (0);
+}
