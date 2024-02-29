3a192dcd0bd70b7613a931e8bc2f543e7bdba291
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-02-29 13:50:55 +0100

    task 14 fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_is_full - checks if a binary tree is full
+* binary_tree_balance - checks if a binary tree is full
 *
 * @tree: Pointer to the root node of the tree to travers
 *
 * Return: If tree is NULL, your function must return 0
 */
-int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
 	int balance = 0;
 
