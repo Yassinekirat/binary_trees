0edadcda405b2164b02f061ec5e23300b5c54271
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-02-29 12:30:08 +0100

    task 14. Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,12 +1,20 @@
 #include "binary_trees.h"
+
 /**
-* binary_tree_balance - measures the balance factor of a binary tree
-* @tree: Pointer to the root node of the tree to traverse.
+* binary_tree_is_full - checks if a binary tree is full
 *
-* Return: If tree is NULL, return 0
+* @tree: Pointer to the root node of the tree to travers
+*
+* Return: If tree is NULL, your function must return 0
 */
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int balance = 0;
+
 	if (!tree)
 		return (0);
+	balance = binary_tree_height(tree->left) - binary_tree_height(tree->right);
+
+	return (balance);
+
 }
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -3,12 +3,11 @@
 /**
 * binary_tree_is_full - checks if a binary tree is full
 *
-* @tree: Pointer to the root node of the tree to travers
+* @tree: pointer to the root node of the tree to check
 *
-* Return: If tree is NULL, your function must return 0
+* Return: 1 if full, otherwise 0
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	return (0);
 }
