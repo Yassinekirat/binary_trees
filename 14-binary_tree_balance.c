5375e69912f6f35cfbd02f416b2e58ef9da8e11f
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-02-29 13:53:16 +0100

    checker??

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_balance - checks if a binary tree is full
+* binary_tree_balance -  measures the balance factor of a binary tree
 *
-* @tree: Pointer to the root node of the tree to travers
+* @tree: Pointer to the root node of the tree to traverse.
 *
-* Return: If tree is NULL, your function must return 0
+* Return: If tree is NULL, return 0
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
